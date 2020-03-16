e23cc23509da8d0d36e974508a02e7b3e5c77a6a
Guilherme Santos <gui07015@gmail.com>
2020-03-16 08:04:40 -0300

    feat: start tests

---
+++ 05-testes/01/service.js
@@ -0,0 +1,18 @@
+const { get } = require("axios");
+
+const URL = `https://swapi.co/api/people`;
+
+async function obterPessoa(nome) {
+  const url = `${URL}/?search=${nome}&format=json`;
+  const result = await get(url);
+  return result.data.results.map(mapearPessoa);
+}
+
+function mapearPessoa(item) {
+  return {
+    nome: item.name,
+    peso: item.height
+  };
+}
+
+module.exports = { obterPessoa };
